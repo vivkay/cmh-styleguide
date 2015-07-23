8da5dd925c89655c6aec94a18372b0132501f918
Viv Kay <vivkay8@gmail.com>
2015-07-23 16:04:19 -0400

    adds type and opacity shapes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+
+    $('#nav-icon3').click(function(){
         $(this).toggleClass('open');
+        $('.nav-bar').toggleClass('js-nav-open'); 
     });
-});
