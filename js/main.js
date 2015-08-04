67d6dac23c65fe915016e7f78c86f3d04922a00f
Viv Kay <vivkay8@gmail.com>
2015-08-04 16:28:10 -0400

    adds transitions on navigation panel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+var $navBtnIntro = $('.nav-btn-intro');
+var $navPanelIntro = $('.nav-panel-intro');  
+var $navBtnStyle = $('.nav-btn-style'); 
+var $navPanelStyle = $('.nav-panel-style');
 
     $('#nav-icon3').click(function(){
         $(this).toggleClass('open');
         $('.nav-bar').toggleClass('js-nav-open'); 
+    });
+
+    $navBtnIntro.on('click', function () {
+    	$navPanelIntro.toggleClass('js-panel-open');
+    });
+
+    $navBtnStyle.on('click', function () {
+    	$navPanelStyle.toggleClass('js-panel-open');
     });
